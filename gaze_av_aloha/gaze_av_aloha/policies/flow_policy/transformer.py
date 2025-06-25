2efc4cdf66d25055ffa4a1ff406c12e8edc63be3
Ian Chuang <iantc104@gmail.com>
2025-06-25 09:56:31 -0700

    pool2dposemb

--- gaze_av_aloha/gaze_av_aloha/policies/flow_policy/transformer.py
+++ gaze_av_aloha/gaze_av_aloha/policies/flow_policy/transformer.py
@@ -3,6 +3,7 @@ from torch import nn, Tensor
 import math
 import torch.nn.functional as F
 from timm.layers import Mlp
+import numpy as np
 
 class RotaryPosEmbed(nn.Module):
     """ RoPE implementation from torchtune """
@@ -301,64 +302,31 @@ class DiT(nn.Module):
         return x
     
 class AttentionBlock(nn.Module):
-    def __init__(self, hidden_size, num_heads, mlp_ratio=4.0, **block_kwargs):
+    def __init__(self, hidden_size, num_heads, mlp_ratio=4.0, dropout=0.0):
         super().__init__()
-        # self.use_xattn = use_xattn
-        # self.norm1 = nn.LayerNorm(hidden_size)
-        # self.attn = Attention(hidden_size, num_heads=num_heads, **block_kwargs)
-        # if use_xattn:
+        self.norm1 = nn.LayerNorm(hidden_size)
+        self.attn = nn.MultiheadAttention(hidden_size, num_heads, dropout=dropout, batch_first=True)
         self.norm2 = nn.LayerNorm(hidden_size)
-        self.xattn = CrossAttention(hidden_size, num_heads=num_heads, **block_kwargs)
+        self.xattn = nn.MultiheadAttention(hidden_size, num_heads, dropout=dropout, batch_first=True)
         self.norm3 = nn.LayerNorm(hidden_size)
-        self.mlp = Mlp(in_features=hidden_size, hidden_features=int(hidden_size * mlp_ratio), drop=0)
+        self.mlp = Mlp(in_features=hidden_size, hidden_features=int(hidden_size * mlp_ratio), drop=dropout)
 
-    def forward(self, x, c=None):
+    def forward(self, x, c, c_pos_emb=None):
         """
         x: [B, S, D] - Query input
         context: [B, S_ctx, D] - Key/Value input
         mask: Optional attention mask
         """
-        # x = x + self.attn(self.norm1(x))
-        # if self.use_xattn:
-        #     assert c is not None, "Cross-attention requires conditioning context"
-        x = x + self.xattn(self.norm2(x), c)
+        q = k = v = self.norm1(x)
+        x = x + self.attn(query=q, key=k, value=v, need_weights=False)[0]
+        q = self.norm2(x)  
+        k = c + c_pos_emb if c_pos_emb is not None else c
+        v = c
+        x = x + self.xattn(query=q, key=k, value=v, need_weights=False)[0]
         x = x + self.mlp(self.norm3(x))
         return x
 
-class TransformerEncoder(nn.Module):
-    """
-    Transformer encoder with multiple attention blocks.
-    """
-    def __init__(self, hidden_size, num_layers, num_heads, mlp_ratio=4.0, dropout=0.0):
-        super().__init__()
-        self.layers = nn.ModuleList([
-            AttentionBlock(
-                use_xattn=False,
-                hidden_size=hidden_size,
-                num_heads=num_heads,
-                mlp_ratio=mlp_ratio,
-                attn_drop=dropout,
-                proj_drop=dropout,
-            ) for _ in range(num_layers)
-        ])
-        self.initialize_weights()
 
-    def initialize_weights(self):
-        # Initialize transformer layers:
-        def _basic_init(module):
-            if isinstance(module, nn.Linear):
-                torch.nn.init.xavier_uniform_(module.weight)
-                if module.bias is not None:
-                    nn.init.constant_(module.bias, 0)
-            elif isinstance(module, nn.LayerNorm):
-                nn.init.constant_(module.bias, 0)
-                nn.init.constant_(module.weight, 1.0)
-        self.apply(_basic_init)
-
-    def forward(self, x):
-        for layer in self.layers:
-            x = layer(x)
-        return x
     
 class AttentionPooling(nn.Module):
     """
@@ -379,8 +347,7 @@ class AttentionPooling(nn.Module):
                 hidden_size=hidden_size,
                 num_heads=num_heads, 
                 mlp_ratio=mlp_ratio,
-                attn_drop=dropout,
-                proj_drop=dropout,
+                dropout=dropout,
             ) for i in range(depth)
         ])
         # self.proj = nn.Linear(hidden_size, self.intermediate_dim)
@@ -398,7 +365,7 @@ class AttentionPooling(nn.Module):
                 nn.init.constant_(module.weight, 1.0)
         self.apply(_basic_init)
 
-    def forward(self, c):
+    def forward(self, c, c_pos_emb=None):
         """
         context: (B, N, D) - Input features, where B is batch size, N is sequence length, and D is feature dimension.
         """
@@ -406,7 +373,9 @@ class AttentionPooling(nn.Module):
         x = self.query_tokens.weight.unsqueeze(0).expand(B, -1, -1)
         c = self.norm1(c)
         for block in self.blocks:
-            x = block(x=x, c=c)
+            x = block(x=x, c=c, c_pos_emb=c_pos_emb)
         # x = self.proj(x)  # (B, num_queries, intermediate_dim)
         x = self.norm2(x)
         return x # .flatten(start_dim=1) 
+    
+
