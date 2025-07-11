ada0fe028afc3b293ae15a67aa617eee6ffffa78
Ian Chuang <iantc104@gmail.com>
2025-07-10 19:56:58 -0700

    FINAL FINAL FINAL

--- gaze_av_aloha/gaze_av_aloha/policies/gaze_policy/vit.py
+++ gaze_av_aloha/gaze_av_aloha/policies/gaze_policy/vit.py
@@ -202,8 +202,8 @@ def create_vit_s(num_tokens, patch_size) -> ImageEncoder:
         embedding_dim=384,
         num_heads=6,
         act_layer=torch.nn.GELU,
-        drop=0.1,
-        drop_path=0.0,
+        drop=0.0,
+        drop_path=0.1,
     )
 
 def create_vit_b(num_tokens, patch_size) -> ImageEncoder:
@@ -215,7 +215,7 @@ def create_vit_b(num_tokens, patch_size) -> ImageEncoder:
         embedding_dim=768,
         num_heads=12,
         act_layer=torch.nn.GELU,
-        drop=0.1,
-        drop_path=0.0,
+        drop=0.0,
+        drop_path=0.1,
     )
 
