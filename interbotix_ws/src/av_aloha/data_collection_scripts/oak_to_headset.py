89121a48acd22fd112bb88b92498c4e4f9a45e94
Devi Amarsaikhan <devamar@ucdavis.edu>
2026-05-01 23:03:41 -0700

    Updated code to stream oak cam and display controller and headset positions

--- interbotix_ws/src/av_aloha/data_collection_scripts/oak_to_headset.py
+++ interbotix_ws/src/av_aloha/data_collection_scripts/oak_to_headset.py
@@ -1,12 +1,24 @@
+'''
+Stream video from your OAK camera to the headset and overlay controller pose (relative to headset frame) as text.
+'''
+
 import depthai as dai
 import time
 import cv2
 import numpy as np
 from webrtc_headset import WebRTCHeadset
+from headset_control import HeadsetFullControl as HeadsetControl
+from headset_utils import HeadsetFeedback
+from transform_utils import (
+    pose2mat,
+)
 
 # ---- Setup headset ----
 headset = WebRTCHeadset()
 headset.run_in_thread()
+headset_control = HeadsetControl()
+feedback = HeadsetFeedback()
+headset_control.reset()
 
 # ---- Setup pipeline ----
 pipeline = dai.Pipeline()
@@ -38,7 +50,33 @@ while pipeline.isRunning():
     left_img = q_left.get().getCvFrame()
     right_img = q_right.get().getCvFrame()
 
-    # left_img = np.ascontiguousarray(left_img)
-    # right_img = np.ascontiguousarray(right_img)
-
     headset.send_images(left_img, right_img)
+
+    headset_data = headset.receive_data()
+
+    if headset_data is None:
+        continue 
+
+    # --- Extract positions ---
+    h_pos = headset_data.h_pos
+    l_pos = headset_data.l_pos
+    r_pos = headset_data.r_pos
+
+    # --- Compute naive relative ---
+    l_rel = l_pos - h_pos
+    r_rel = r_pos - h_pos
+
+    # --- Build string ---
+    feedback.info = (
+        f"HEAD\n"
+        f"x: {h_pos[0]:.3f} y: {h_pos[1]:.3f} z: {h_pos[2]:.3f}\n\n"
+        f"LEFT\n"
+        f"x: {l_pos[0]:.3f} y: {l_pos[1]:.3f} z: {l_pos[2]:.3f}\n\n"
+        f"RIGHT\n"
+        f"x: {r_pos[0]:.3f} y: {r_pos[1]:.3f} z: {r_pos[2]:.3f}\n\n"
+        f"REL\n"
+        f"L-H: ({l_rel[0]:.3f}, {l_rel[1]:.3f}, {l_rel[2]:.3f})\n"
+        f"R-H: ({r_rel[0]:.3f}, {r_rel[1]:.3f}, {r_rel[2]:.3f})"
+    )
+
+    headset.send_feedback(feedback)
